5fae181e692ccf5d9782f9bd5dd7e376e94ade62
Prem Patel <prempatel_8@outlook.com>
2021-07-12 18:39:20 -0500

    1.10: unicafe step5

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -20,36 +20,41 @@ const App = () => {
     setAll(all + 1);
   };
 
-  return (
-    <div>
-      <Header name="give feedback" />
-
-      <Button handleClick={giveGoodFeedback} text="good" />
-      <Button handleClick={giveNeutralFeedback} text="neutral" />
-      <Button handleClick={giveBadFeedback} text="bad" />
-
-      <Header name="statistics" />
-
-      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
-    </div>
-  );
-};
-
-const Statistics = ({ good, neutral, bad, all }) => {
   if (good === 0 && neutral === 0 && bad === 0) {
-    return <div>No feedback given</div>;
+    return (
+      <div>
+        <Header name="give feedback" />
+        <Button handleClick={giveGoodFeedback} text="good" />
+        <Button handleClick={giveNeutralFeedback} text="neutral" />
+        <Button handleClick={giveBadFeedback} text="bad" />
+        <Header name="statistics" />
+        <div>No feedback given</div>
+      </div>
+    );
   } else {
     return (
-      <>
-        <Display name="good" value={good} />
-        <Display name="neutral" value={neutral} />
-        <Display name="bad" value={bad} />
-        <Display name="all" value={good + neutral + bad} />
-        <Display name="average" value={(good - bad) / all} />
-        <Display name="positive" value={(good / all) * 100} />
-      </>
+      <div>
+        <Header name="give feedback" />
+
+        <Button handleClick={giveGoodFeedback} text="good" />
+        <Button handleClick={giveNeutralFeedback} text="neutral" />
+        <Button handleClick={giveBadFeedback} text="bad" />
+
+        <Header name="statistics" />
+
+        <Statistics text="good" value={good} />
+        <Statistics text="neutral" value={neutral} />
+        <Statistics text="bad" value={bad} />
+        <Statistics text="all" value={all} />
+        <Statistics text="average" value={(good - bad) / all} />
+        <Statistics text="positive" value={(good / all) * 100} />
+      </div>
     );
   }
+};
+
+const Statistics = ({ text, value }) => {
+  return <Display text={text} value={value} />;
 };
 
 const Header = ({ name }) => {
@@ -60,27 +65,27 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
-const Display = ({ name, value }) => {
+const Display = ({ text, value }) => {
   const nameSet = new Set(["all", "average", "positive"]);
 
   if (
-    nameSet.has(name) &&
+    nameSet.has(text) &&
     (value === "undefined" || value == null || isNaN(value) || value < 0)
   ) {
     value = 0;
   }
 
-  if (name === "positive") {
+  if (text === "positive") {
     return (
       <span>
-        {name} {value} %
+        {text} {value} %
         <br />
       </span>
     );
   } else {
     return (
       <span>
-        {name} {value}
+        {text} {value}
         <br />
       </span>
     );
